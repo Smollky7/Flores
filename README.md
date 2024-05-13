a91bddef4581323638d141ebc2763e603459c017
Jardel Sousa <70775491+Smollky7@users.noreply.github.com>
2024-05-13 12:37:52 -0300

    working

---
+++ README.md
@@ -0,0 +1,23 @@
+# Presente Especial para Minha Namorada
+
+Este é um presente especial que criei para você, minha querida [nome da sua namorada]. Espero que goste! ❤️
+
+## Sobre o Presente
+
+Este presente é uma animação de flores com uma mensagem especial "EU TE AMO", acompanhada por um botão pulsante vermelho que a levará de volta ao início.
+
+## Instruções
+
+1. Abra o arquivo `index.html` em um navegador da web.
+2. Aguarde a animação das flores carregar.
+3. Após a animação das flores, uma mensagem especial "EU TE AMO" aparecerá acima delas.
+4. Um botão pulsante "Voltar ao Início" aparecerá na parte inferior da página.
+5. Clique no botão para retornar ao início da animação.
+
+## Como Executar
+
+Basta abrir o arquivo `index.html` em qualquer navegador da web moderno, como Google Chrome, Mozilla Firefox, ou Safari.
+
+## Agradecimentos
+
+Obrigado por estar sempre ao meu lado. Este presente é apenas uma pequena expressão do meu amor por você.
---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+window.onload = () => {
+  document.body.classList.remove("container");
+  
+  // Seleciona o elemento da mensagem de amor
+  const loveMessage = document.querySelector('.love-message');
+  
+  // Define uma função para adicionar a classe de animação à mensagem de amor
+  const showLoveMessage = () => {
+    loveMessage.classList.add('show');
+  };
+  
+  // Espera 3.8 segundos antes de mostrar a mensagem de amor
+  setTimeout(showLoveMessage, 3800);
+  
+  // Seleciona o botão de voltar
+  const backButton = document.querySelector('.back-to-index');
+  
+  // Define uma função para mostrar o botão de voltar após um certo tempo
+  const showBackButton = () => {
+    backButton.style.opacity = "1";
+  };
+  
+  // Espera 3.8 segundos antes de mostrar o botão de voltar
+  setTimeout(showBackButton, 3800);
+  
+  // Reproduz a música a partir do minuto desejado
+  const audio = document.getElementById('myAudio');
+  audio.currentTime = 40;
+  audio.play();
+};
